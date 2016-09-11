ffbcad3875b1bbbe440588fb02bf65f05aecadaf
matiaspro3 <matias9387@hotmail.com>
2016-09-11 01:24:35 -0300

    V1.0.1

---
+++ V.1.0.1/www/js/controllers.js
@@ -0,0 +1,118 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $ionicPopup, $state) {
+  $scope.usuario = {};
+  $scope.usuario.nombre = "";
+  $scope.login = function() {
+      if($scope.usuario.nombre == "")
+        $scope.showAlert("POR FAVOR INGRESE SU NOMBRE");
+      else{
+        $scope.showAlert("BIENVENIDO " + $scope.usuario.nombre + "!");
+        var usuario = { "nombre": $scope.usuario.nombre};
+        $state.go('tab.jugar', usuario);
+      }
+    };
+
+    $scope.showAlert = function(resultado) {
+      var alertPopup = $ionicPopup.alert({
+         title: resultado
+      });
+      alertPopup.then(function(res) {
+         // Custom functionality....
+      });
+   };
+})
+
+.controller('JugarCtrl', function($scope, $ionicPopup, $state, $stateParams, $cordovaVibration, Preguntas, Respuestas, Opciones ) {
+
+  $scope.nombreUsuario = angular.fromJson($stateParams);
+  $scope.showComenzar = true;
+  $scope.preguntas = Preguntas;
+  $scope.respuestas = Respuestas;
+  $scope.opciones = Opciones;
+  $scope.btnOp1Estado = 'clear';
+  $scope.btnOp2Estado = 'clear';
+  $scope.btnOp3Estado = 'clear';
+
+  $scope.random = Math.round(Math.random() * 2); //TODO: Cambiar el random a un tanaño variable de a cuerdo a la cantidad de preguntas cargadas en firebase
+
+  $scope.getPregunta = function() {
+    if($scope.nombreUsuario.nombre == 'NOLOGUEADO'){
+    $scope.showAlert("No se ha logueado!");
+    $state.go('tab.login');
+  }else{
+    $scope.showComenzar = false;
+    $scope.showPregunta = true;
+  }
+  };
+
+  $scope.setRespuesta = function(idOpcion, Respuesta, btnApretado) {
+    if(idOpcion == Respuesta){
+      try{
+      $cordovaVibration.vibrate(100);  
+      }catch(err){
+        alert("No es un dispositivo mobile" + err.message);
+      }
+      $scope.cambiarColorBoton(btnApretado, 'correcto');
+    //Le agrego un retardo para que me muestre el popUp del resultado y me muestre la próxima pregunta
+    setTimeout(function() {
+      $scope.showAlert("CORRECTO!", btnApretado);
+      }, 700);  
+    }
+    else{
+      try{
+        var patron = [100, 100, 100, 100];
+      $cordovaVibration.vibrate(patron); 
+      }catch(err){
+        alert("No es un dispositivo mobile" + err.message);
+      }
+      $scope.cambiarColorBoton(btnApretado, 'incorrecto');
+    
+      setTimeout(function() {
+        $scope.showAlert("INCORRECTO!", btnApretado);
+      }, 700);
+    }
+  };
+
+
+  $scope.cambiarColorBoton = function(btnApretado, estado) {
+  switch(btnApretado){
+        case 'btnOp1':
+          $scope.btnOp1Estado = estado;
+        break;
+        case 'btnOp2':
+          $scope.btnOp2Estado = estado;
+        break;
+        case 'btnOp3':
+          $scope.btnOp3Estado = estado;
+        break;
+        default:
+        break;
+      }
+  }
+
+  $scope.showAlert = function(resultado, btnApretado) {
+  
+      var alertPopup = $ionicPopup.alert({
+         title: resultado,
+         okText: "SIGUIENTE"
+      });
+
+      alertPopup.then(function(res) {
+        //vuelvo a poner el boton en el color por default
+        $scope.cambiarColorBoton(btnApretado, 'clear'); 
+        //recargo la variable random para que se recargue la siguiente pregunta
+         $scope.random = Math.round(Math.random() * 2); 
+      });
+   };
+
+
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AcercadeCtrl', function($scope) {
+  $scope.miFoto = 'img/perfil.png';
+});
